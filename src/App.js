23040bfc1a615778874887780e523d32f72a733e
Your Name <you@example.com>
2021-12-06 16:55:10 +0600

    hello programmer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="header">This is my simple rest countries data load to an API...</h2>
+      <CoutryLoad></CoutryLoad>
     </div>
   );
 }
 
+//this compunent data load to an api....
+function CoutryLoad() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const URL = `https://restcountries.com/v3.1/all`;
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+  }, [])
+
+  return (
+    <div>
+      <div className="mainDiv">
+        {countries.map((country) => <DisplayCountries data={country} key={country.name.common}></DisplayCountries>)}
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '30px', marginBottom: '30px' }}>All rights reversed.Copyright © 2021 Habibor-Rahaman.com</div>
+    </div>
+
+  )
+}
+
+
+//this conponent my data to display in the ui....
+function DisplayCountries(props) {
+  const { flags, name, flag, region, population, maps, independent, capital, area, status, timezones } = props.data;
+  const timezone = timezones[0];
+  return (
+    <div className="childDiv">
+      <img src={flags.svg ? flags.svg : 'Empty Data'} alt="country" />
+      <p>Official Name: {name.official ? name.official : 'Empty Data'} <span>{flag ? flag : 'Empty Data'}</span></p>
+      <p>Common Name: {name.common ? name.common : 'Empty Data'} <span>{flag ? flag : 'Empty Data'}</span></p>
+      <p>Capital: {capital ? capital : 'Empty Data'}</p>
+      <p>Area : {area ? area : 'Empty Data'}</p>
+      <p>Population: {population ? population : 'Empty Data'}</p>
+      <p>Timezone: {timezone ? timezone : 'Empty Data'}</p>
+      <p>Independent: {independent ? 'Trure' : 'False'}</p>
+      <p>Region: {region ? region : 'Empty Data'}</p>
+      <p>Status: {status ? status : 'Empty Data'}</p>
+      <p>Location: <a href={maps.googleMaps ? maps.googleMaps : 'Empty Data'} target="_blank" rel='noreferrer'> google map of county</a>  </p>
+    </div>
+  )
+}
 export default App;
